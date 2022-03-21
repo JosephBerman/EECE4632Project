9fbf5cfa2289541f87852539204379a63678f803
Joseph Berman <69022719+JosephBerman@users.noreply.github.com>
2022-03-20 22:36:40 -0400

    fixed code

--- encryption.py
+++ encryption.py
@@ -1,5 +1,5 @@
 import random
-
+import math
 
 def gcd(a, b):
     while b != 0:
@@ -20,10 +20,11 @@ def findLCM(num1, num2):
         greater = num2
 
     while (True):
-        if ((greater % num1 == 0) and (greater % num2 == 0)):
+        if (greater % num1 == 0) and (greater % num2 == 0):
             lcm = greater
             break
         greater += 1
+
     return lcm
 
 
@@ -41,61 +42,50 @@ def primesInRange(x, y):
     return prime_list
 
 
-def modinv(u, v):
-    # Step X1. Initialise
-    u1 = 1
-    u3 = u
-    v1 = 0
-    v3 = v
-    # Remember odd/even iterations
-    iter = 1
-    # Step X2. Loop while v3 != 0
-    while v3 != 0:
-        # Step X3. Divide and "Subtract"
-        q = u3 / v3
-        t3 = u3 % v3
-        t1 = u1 + q * v1
-        # Swap
-        u1 = v1;
-        v1 = t1;
-        u3 = v3;
-        v3 = t3
-        iter = -iter
-
-    # Make sure u3 = gcd(u,v) == 1
-    if u3 != 1:
-        return 0;  # Error: No inverse exists
-    # Ensure a positive result
-    if iter < 0:
-        inv = v - u1
-    else:
-        inv = u1
-    return inv
 
 
-# primes_list = primesInRange(300, 400)
+def modInv(u, v):
+    for x in range(1, v):
+        if ((u % v) * (x % v) % v == 1):
+            return x
 
-q = 53  # random.choice(primes_list)	 first prime number
-p = 61  # random.choice(primes_list)	 second prime number
+def getCoprime(lcm):
+    for i in range(2, lcm):
+        if isCoprime(i, lcm) == True:
+            return i
+
+
+def getTotient(x, y):
+    return (x-1) * (y-1)
+
+def getDval(phi, e):
+    for i in range(e * 25):
+        if (e * i) % phi == 1:
+            return i
+
+primes_list = primesInRange(200, 600)
+
+q = random.choice(primes_list)#	 first prime number
+p = random.choice(primes_list)	 #second prime number
 
 n = p * q  # n is the product of the two generated prime numbers
 print("p: ", p)
 print("p: ", q)
 print("n: ", n)
 
+phi = getTotient(p, q)
+print("phi: ", phi)
 lcm = findLCM(p, q)
 print("lcm: ", lcm)
 
-for i in range(2, lcm):
-    if isCoprime(i, lcm) == True:
-        coprime = i
-        break
 
-coprime = 17
+coprime = getCoprime(lcm)
+
+#coprime = 17
 
 print("coprime:", coprime)
-
-d = modinv(n, coprime)
+phi = getTotient(p, q)
+d = modInv(coprime, phi)  #modinv(phi, coprime)
 print("d: ", d)
 
 message = 69
