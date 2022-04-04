a10bfedda12a7c6b3d30e4e7429dbc7da1a47210
Jassiel Deliz <jassieldeliz@gmail.com>
2022-04-04 14:53:17 -0400

    Testbench for encryption.cpp

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ap_int.h>
+#include <math.h>
+
+using namespace std;
+
+int test();
+
+int main()
+{
+	cout << "\n********************************************\n" << " Starting ..." << "\n";
+
+	test();
+
+return 0;
+}
