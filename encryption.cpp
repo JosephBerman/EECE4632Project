e3dea9d242711338efb28278dfd04f7aa6d3dbda
Joseph Berman <69022719+JosephBerman@users.noreply.github.com>
2022-04-11 16:10:41 -0400

    Add files via upload

--- encryption.cpp
+++ encryption.cpp
@@ -1,104 +1,182 @@
-#include <ap_int.h>
-
 #include <iostream>
-//#include <stdlib.h>
-//#include <time.h>
 #include <math.h>
+#include <ap_int.h>
 
 
 using namespace std;
+typedef ap_uint<1024> edmessage;
 
 #define BITS 16
 
-typedef ap_uint<1024> edmessage;
-
-int gcd(int a, int b) {
-
-	int c;
-	while(b != 0){
-		c = a&b;
-		a = b;
-		b = c;
-
-	}
-
-
-	return a;
-
-}
-
-int isCoprime(int e, int lcm) {
-	return gcd(e, lcm) == 1;
-}
-
-int findLCM(int a, int b) {
-	int greater;
-	int lcm;
-	a -= 1;
-	b -= 1;
-
-	if(a > b) {
-		greater = a;
-	} else {
-		greater = b;
-	}
-	while(true) {
-		if(((greater % a) == 0) && ((greater % b) == 0)) {
-			lcm = greater;
-			break;
-		}
-		greater++;
-	}
-	return lcm;
-}
+int getGDC(int a, int b);
+int main();
+int modinv(int a, int m);
+int modexp(edmessage base, edmessage exp, int modulo);
+bool isCoprime(int e, int lcm);
+int getCoprime(int lcm);
+int getTotient(int x, int y);
+int findLCM(int a, int b);
+bool isPrime(int n);
+edmessage edPow(edmessage base, int toPow);
 
 
-int modInv(int u, int v) {
-	for(int i = 1; i < v; i++) {
-		if((u % v) * (i % v) % v == 1) {
-			return i;
-		}
-	}
-}
-
-int getCoprime(int lcm) {
-	for(int i = 2; i < lcm; i++) {
-		if (isCoprime(i, lcm) == true) {
-			return i;
-		}
-	}
-}
-
-int getTotient(int x, int y) {
-	return (x - 1) * (y - 1);
-}
-
-int getDval(int phi, int e) {
-	for(int i = 0; i < e*25; i++) {
-		if((e * i) % phi == 1) {
-			return i;
-		}
-	}
-}
-/*
+/**
+ * @brief
+ *
+ * @param n
+ * @return true
+ * @return false
+ */
 bool isPrime(int n) {
-    int flag = 0;
-    for (int i = 2; i <= n / i; ++i) {
-        if (n % i == 0) {
+    if(n == 0 || n == 1) {
+        return false;
+    }
+
+    for(int i = 2; i <= n/2; i++) {
+        if(n % i == 0) {
             return false;
         }
+
     }
     return true;
 }
-int generatePrimeNum(int numBits) {
-	int num;
-    int bit_num = pow(2,numBits);
-	do {
-		num = rand() % bit_num;
-	} while (!isPrime(num));
-	return num;
+
+
+/**
+ * @brief
+ *
+ * @param a
+ * @param b
+ * @return int
+ */
+int getGDC(int a, int b) {
+    int c;
+    while(b != 0) {
+        c = a % b;
+        a = b;
+        b = c;
+    }
+    return a;
 }
-*/
+
+/**
+ * @brief
+ *
+ * @param a
+ * @param m
+ * @return int
+ */
+int modinv(int a, int m) {
+    int m0 = m;
+    int y = 0;
+    int x = 1;
+    if(m == 1) {
+        return 0;
+    }
+
+    while(a > 1) {
+        int q = a / m;  //calculate the quotient
+        int t = m;
+
+        m = a % m, a = t;
+        t = y;
+
+        y = x - q * y;
+        x = t;
+    }
+    if(x < 0) {
+        x += m0;
+    }
+    return x;
+}
+
+/**
+ * @brief
+ *
+ * @param base
+ * @param exp
+ * @param modulo
+ * @return int
+ */
+int modexp(edmessage base, edmessage exp, int modulo) {
+	edmessage sol = 1;
+    while(exp > 0) {
+        //cout << "base: " << base << endl;
+        //cout << "exp: " << exp << endl;
+        //cout << "modulo: " << modulo << endl;
+
+        if(exp % 2 == 1) {
+            //cout << "sol = " << sol << " * " << base << " % " << modulo << endl;
+            sol = (sol * base) % modulo;
+        }
+        exp = exp >> 1;
+        //cout << "base = " << base << " * " << base << " % " << modulo << endl;
+        edmessage base = (base * base) % modulo;
+        //cout << "new base: " << base << endl;
+        //cout << "--------------------------------" << endl;
+
+    }
+    return sol;
+}
+
+/**
+ * @brief
+ *
+ * @param e
+ * @param lcm
+ * @return true
+ * @return false
+ */
+bool isCoprime(int e, int lcm) {
+    if(isPrime(e)) {
+        return getGDC(e, lcm) == 1;
+    }
+}
+
+/**
+ * @brief Calculate a coprime of the least common factor
+ *
+ * @param lcm Least common factor
+ * @return A coprime of lcm
+ */
+int getCoprime(int lcm) {
+    for(int i = lcm; i > 2; i--) {
+        if(isCoprime(i, lcm) == true) {
+            return i;
+        }
+    }
+}
+
+/**
+ * @brief Calculates the least common multiple of a and b
+ *
+ * @param a First integer input
+ * @param b Second integer input
+ * @return The least common multiple of a and b
+ */
+int findLCM(int a, int b) {
+    int greater;
+    int lcm;
+
+    a -= 1;
+    b -= 1;
+
+    if(a > b) {
+        greater = a;
+    } else {
+        greater = b;
+    }
+    while (true)
+    {
+        if(greater % a == 0 && greater % b == 0) {
+            lcm = greater;
+            break;
+        }
+        greater++;
+    }
+    return lcm;
+}
+
 
 edmessage edPow(edmessage base, int toPow){
 	edmessage count;
@@ -108,36 +186,46 @@ edmessage edPow(edmessage base, int toPow){
 	}
 	return count;
 }
-int test() {
-    //srand(time(NULL));
-
-    int q = 11;
-    int  p = 7;
-
-    int n = p * q;
 
 
-    int phi = getTotient(p, q);
+int test(int message, int &privateKey, int &publicKey, int &n) {
+    int p = 11;
+    int q = 17;
+
+    cout << "p: " << p << " q: " << q << endl;
+
+     n = p * q;
+    cout << "n: " << n << endl;
 
     int lcm = findLCM(p, q);
+    cout << "lcm: " << lcm << endl;
 
-    int coprime = getCoprime(lcm);
+    int coprime = 23; //getCoprime(lcm);
+    cout << "coprime: " << coprime << endl;
 
-    int d = modInv(coprime, phi);
-
-
+    int d = modinv(coprime, lcm);
     cout << "d: " << d << endl;
-    edmessage message = 6;
-
-    cout << "Message: " << message << endl;
-   // edmessage encryptedMessage = fmod((edmessage)pow(message, coprime), n);
-    edmessage encryptedMessage = edPow((edmessage)message, coprime) % n;
-    cout << "Encrypted Message: " << encryptedMessage << endl;
+    privateKey = d;
+    publicKey = coprime;
+    cout << "Original message: " << message << endl;
 
 
-  //  edmessage decryptedMessage = fmod((edmessage)pow(encryptedMessage, d),n);
-    edmessage decryptedMessage = edPow(encryptedMessage, d) % n;
-    cout << "Decrypted Message: " << decryptedMessage << endl;
+    cout << "String message converted to ints: ";
 
 
+    cout << endl;
+
+
+
+     cout << "Message: " << message << endl;
+     // edmessage encryptedMessage = fmod((edmessage)pow(message, coprime), n);
+    int  encryptedMessage = edPow((edmessage)message, coprime) % n;
+     cout << "Encryptd Message: " << encryptedMessage << endl;
+
+
+    //  edmessage decryptedMessage = fmod((edmessage)pow(encryptedMessage, d),n);
+      edmessage decryptedMessage = edPow(encryptedMessage, d) % n;
+      cout << "Decrypted Message: " << decryptedMessage << endl;
+
+      return encryptedMessage;
 }
